3b5eb335a471402caefd8ebe2ce528bd640744f7
darkob-code <87665820+darkob-code@users.noreply.github.com>
2023-01-30 22:23:59 +0100

    Add files via upload

--- Test.py
+++ Test.py
@@ -4,8 +4,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-
-
 os.environ['PATH'] += "c:\Selenium_Driver" #putanja do webdrivera, svaki browser ima svoj driver kojeg treba skinuti i treba paziti na verziju browsera, mora biti ista kao driver
 driver = webdriver.Chrome()
 driver.get("https://jqueryui.com/resources/demos/progressbar/download.html")
@@ -14,7 +12,11 @@ driver.implicitly_wait(30)
 my_element = driver.find_element(By.ID, "downloadButton")#desni klik na gumb, provjeri kod, naziv button id-a
 my_element.click() #automatski klik na gumb Start Download
 
+WebDriverWait(driver, 30).until(
+    EC.text_to_be_present_in_element(
+        (By.CLASS_NAME, 'progress-label'), #filtracija elementa
+        'Complete'  # ocekivani text
+    )
+)
 
 
-progress_element = driver.find_element(By.ID, "")#namjerno ostavljena greška jer driver zatvara browser, nisam nasao rjesenje, nema ni na forumima
-my_element.click()
