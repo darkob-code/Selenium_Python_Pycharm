a422861904f03c1460de7bd070a14ab49e808815
darkob-code <87665820+darkob-code@users.noreply.github.com>
2023-01-30 21:54:43 +0100

    Add files via upload

--- Test.py
+++ Test.py
@@ -1,10 +1,20 @@
 import os
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
 
 os.environ['PATH'] += "c:\Selenium_Driver"
 driver = webdriver.Chrome()
-driver.get("https://www.seleniumeasy.com")
+driver.get("https://jqueryui.com/resources/demos/progressbar/download.html")
 # nakon pokretanja dobivamo poruku Chrome is being controlled by automated test software
 driver.implicitly_wait(30)
-my_element = driver.find_element()#ostavljeno namjerno da se pojavi greška jer omogućuje da se chrome ne isključi odmah. Jer ako nema greške webdriver se isključuje i zatvara browser.
+my_element = driver.find_element(By.ID, "downloadButton")#desni klik na gumb, provjeri kod, naziv button id-a
+my_element.click() #automatski klik na gumb Start Download
+
+
+
+progress_element = driver.find_element(By.ID, "")#namjerno ostavljena greška jer driver zatvara browser, nisam nasao rjesenje, nema ni na forumima
 my_element.click()
